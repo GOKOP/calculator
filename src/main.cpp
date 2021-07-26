359a3cb060b4c1cbd4f2fce9bd31ba51f4539124
GOKOP <igorbugajski@protonmail.com>
2021-07-26 18:05:31 +0200

    continous user input

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 #include <string>
+#include <stdexcept>
+#include <memory>
 
 #include "Parser.hpp"
 #include "Evaluator.hpp"
 
 int main() {
 	std::string input;
-	std::getline(std::cin, input);
 
-	auto parser = Parser(input);
-	auto tree = parser.parse();
+	while(true) {
+		std::getline(std::cin, input);
+	
+		auto parser = Parser(input);
+		std::unique_ptr<ASTNode> tree;
 
-	Evaluator ev;
-	std::cout<<ev.evaluate(tree)<<std::endl;
+		try {
+			tree = parser.parse();
+		} 
+		catch(std::runtime_error e) {
+			std::cout<<e.what()<<std::endl;
+			continue;
+		}
+
+		Evaluator ev;
+		std::cout<<ev.evaluate(tree)<<std::endl;
+	}
 }
