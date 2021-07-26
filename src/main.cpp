c9428da3c60ac8e8a3711c84ff5ed2499aaa3f50
GOKOP <igorbugajski@protonmail.com>
2021-07-26 17:23:43 +0200

    removed TreePrinter

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 
 #include "Parser.hpp"
-#include "TreePrinter.hpp"
 #include "Evaluator.hpp"
 
 int main() {
 	auto parser = Parser("14+22-5+3");
 	auto tree = parser.parse();
-	TreePrinter printer;
-	printer.print(tree);
 	Evaluator ev;
 	std::cout<<ev.evaluate(tree)<<std::endl;
 }
