f8acfb47b7855a9c5b641191b6ea0a95de36f5d2
GOKOP <igorbugajski@protonmail.com>
2021-07-28 03:54:20 +0200

    quitting and help

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,34 @@
 #include "Evaluator.hpp"
 #include "Serializer.hpp"
 
+void print_help() {
+	std::cout<<"Valid operations:\n"
+		"(x and y are numbers, named functions or expressions in parentheses)\n"
+		"+x\n"
+		"-y\n"
+		"x + y\n"
+		"x - y\n"
+		"x * y\n"
+		"x / y\n"
+		"x ^ y\n"
+		"sqrt(x)\n"
+		"cbrt(x)\n"
+		"root(x, y)\n";
+}
+
 int main() {
+	std::cout<<"Type ':q' to quit, ':h' for help"<<std::endl;
 	std::string input;
 
 	while(true) {
 		std::cout<<"> ";
 		std::getline(std::cin, input);
+
+		if(input == ":q") break;
+		if(input == ":h") {
+			print_help();
+			continue;
+		}
 	
 		auto parser = Parser(input);
 		std::unique_ptr<ASTNode> tree;
