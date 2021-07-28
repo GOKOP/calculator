cf76261a00a949d21fc4d96cc961eddb0f904ec1
GOKOP <igorbugajski@protonmail.com>
2021-07-28 16:36:33 +0200

    word tokens now constructed with correct position

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -50,16 +50,16 @@ Token Lexer::get_next_token() {
 		if(isalnum(input[current_pos])) {
 			auto pos = current_pos + 1;
 			auto word = get_word();
-			if(word == "sqrt") return { Token::Sqrt, 0, current_pos+1 };
-			if(word == "cbrt") return { Token::Cbrt, 0, current_pos+1 };
-			if(word == "root") return { Token::Root, 0, current_pos+1 };
-			if(word == "sin") return { Token::Sin, 0, current_pos+1 };
-			if(word == "cos") return { Token::Cos, 0, current_pos+1 };
-			if(word == "tan") return { Token::Tan, 0, current_pos+1 };
-			if(word == "ctg") return { Token::Ctg, 0, current_pos+1 };
-			if(word == "asin") return { Token::Asin, 0, current_pos+1 };
-			if(word == "acos") return { Token::Acos, 0, current_pos+1 };
-			if(word == "atan") return { Token::Atan, 0, current_pos+1 };
+			if(word == "sqrt") return { Token::Sqrt, 0, pos };
+			if(word == "cbrt") return { Token::Cbrt, 0, pos };
+			if(word == "root") return { Token::Root, 0, pos };
+			if(word == "sin") return { Token::Sin, 0, pos };
+			if(word == "cos") return { Token::Cos, 0, pos };
+			if(word == "tan") return { Token::Tan, 0, pos };
+			if(word == "ctg") return { Token::Ctg, 0, pos };
+			if(word == "asin") return { Token::Asin, 0, pos };
+			if(word == "acos") return { Token::Acos, 0, pos };
+			if(word == "atan") return { Token::Atan, 0, pos };
 			errors += "Invalid function '" + word + "' ignored at position " + std::to_string(pos) + "\n";
 			continue;
 		}
